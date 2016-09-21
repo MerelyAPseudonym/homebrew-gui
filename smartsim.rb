9426e816ecc6b1d4db8a7edcf8550568c031bdf7
Josh Tilles <josh@signafire.com>
2016-09-21 09:48:41 -0400

    savegame

--- smartsim.rb
+++ smartsim.rb
@@ -2,7 +2,7 @@ class Smartsim < Formula
   homepage "http://smartsim.org.uk"
   url "https://github.com/ashleynewson/SmartSim/archive/v1.4.tar.gz"
   sha256 "a0b088d69835fc85bb4410d03f8e91cdced90e362e4216797c3bd19ac8484b33"
-  head "https://github.com/ashleynewson/smartsim"
+  head "https://github.com/ashleynewson/smartsim.git"
 
   bottle do
     sha256 "7fbb9ae95a846f794cf6c3d8603bf002374e7a221c59f540a417fa0755eaf282" => :el_capitan
@@ -10,12 +10,16 @@ class Smartsim < Formula
     sha256 "6dc1cbbf12bc1a41f019dc161d33740f80446744d72f180028869c3c476e32f8" => :mavericks
   end
 
+  depends_on "automake" => :build
+  depends_on "autoconf" => :build
+  depends_on "libtool" => :build
+  depends_on "vala" => :build
   depends_on "gtk+3"
   depends_on "librsvg"
-  depends_on "libxml2"
+  # depends_on "libxml2"
   depends_on "glib"
   depends_on "pkg-config" => :build
-  depends_on "gettext"
+  # depends_on "gettext"
 
   def install
     system "./configure", "--disable-dependency-tracking",
@@ -28,5 +32,22 @@ class Smartsim < Formula
   end
 end
 
+<<<<<<< Updated upstream
 # TODO patch `rm --force`
 # TODO patch `sed --expression`
+=======
+__END__
+diff --git a/configure.ac b/configure.ac
+index 90b4e9b..4a5a8fc 100644
+--- a/configure.ac
++++ b/configure.ac
+@@ -4,7 +4,7 @@
+AC_PREREQ([2.68])
+AC_INIT([smartsim], [1.4], [ashleynewson@smartsim.org.uk])
+AC_CONFIG_SRCDIR([config.h.in])
+-AM_INIT_AUTOMAKE
++AM_INIT_AUTOMAKE([subdir-objects])
+AC_CONFIG_HEADERS([config.h])
+
+
+>>>>>>> Stashed changes
